ef217e47209df8999d0c585f019404cdf9229e54
TripleD1337 <danieldegendev@gmail.com>
2024-09-03 09:30:51 +0200

    force color-scheme dark

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,11 @@ export default function Document(props) {
     let pageProps = props.__NEXT_DATA__?.props?.pageProps
 
     return (
-        <Html className="dark h-full scroll-smooth antialiased [font-feature-settings:'ss01']" lang="en">
+        <Html
+            className="dark h-full scroll-smooth antialiased [font-feature-settings:'ss01']"
+            lang="en"
+            style={{ colorScheme: 'dark' }}
+        >
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
