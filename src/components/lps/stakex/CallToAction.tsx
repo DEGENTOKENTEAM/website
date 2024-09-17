50a9429c18b282849882b1529715d31550a150d4
TripleD1337 <danieldegendev@gmail.com>
2024-09-17 19:12:04 +0200

    update

--- src/components/lps/stakex/CallToAction.tsx
+++ src/components/lps/stakex/CallToAction.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@headlessui/react'
 import clsx from 'clsx'
+import { usePathname } from 'next/navigation'
 
 export const CallToAction = () => {
     return (
@@ -13,7 +14,7 @@ export const CallToActionButton = ({ className }: { className?: string }) => {
     return (
         <Button
             as="a"
-            href={`${location.protocol}//${location.host}/dapp/defitools/stakex/create?ref=0x1234`}
+            href={`https://dgnx.finance/dapp/defitools/stakex/create?ref=0x1234`}
             className={clsx([
                 'w-auto cursor-pointer rounded-lg bg-dapp-cyan-500 p-4 font-title text-xl font-bold hover:bg-dapp-cyan-500/40',
                 className,
