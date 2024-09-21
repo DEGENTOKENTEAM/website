cb1613ecf34ae46008caf4c8fadf6aa762b50333
TripleD1337 <danieldegendev@gmail.com>
2024-09-22 00:22:32 +0200

    update Referrer ID

--- src/components/lps/stakex/CallToAction.tsx
+++ src/components/lps/stakex/CallToAction.tsx
@@ -14,7 +14,7 @@ export const CallToActionButton = ({ className }: { className?: string }) => {
     return (
         <Button
             as="a"
-            href={`https://dgnx.finance/dapp/defitools/stakex/create?ref=0x1234`}
+            href={`https://dgnx.finance/dapp/defitools/stakex/create?ref=0xa1276e4d73a99db98acb225f9af69b50dafb8f7b71fd10284db0ca5e6b86fe9f`}
             className={clsx([
                 'w-auto cursor-pointer rounded-lg bg-dapp-cyan-500 p-4 font-title text-xl font-bold hover:bg-dapp-cyan-500/40',
                 className,
